6b0938def062f839524c3cbeaa2b505926bb6f97
Sankha Peiris <lhpei2@student.monash.edu>
2020-05-20 02:45:04 +1000

    - Added movement along path to scenes - Added "around the pond" text - Fixed lighting in scenes

--- rosie-app/Assets/movementRosie.cs
+++ rosie-app/Assets/movementRosie.cs
@@ -1,21 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PathCreation;
 
 public class movementRosie : MonoBehaviour
 {
-    public Vector3 targetPosition;
+    public PathCreator path;
     float start_y;
     float moveSpd=0.02f;
+    float dist=0f;
     float ang_y=0f;
     float temp_ang=0f;
-    Vector3 initial_pos;
+
     // Start is called before the first frame update
     void Start()
     {
-        initial_pos=transform.localPosition;
-        targetPosition=transform.localPosition;
-        targetPosition.x+=0.1f;
         start_y=transform.localPosition.y;
     }
 
@@ -23,10 +22,6 @@ public class movementRosie : MonoBehaviour
     void Update()
     {
         Vector3 pos = transform.localPosition;
-        if (pos.x!=targetPosition.x)
-        {
-            ang_y=(targetPosition.x-pos.x)>0? 0f: 180f; 
-        }
         temp_ang=Mathf.Lerp(temp_ang,ang_y,Mathf.Clamp(Time.deltaTime*5f,0,1));
         transform.localRotation=Quaternion.Euler(0f, temp_ang, 10f/2*Mathf.Sin(Time.time*3f));
         
@@ -35,26 +30,19 @@ public class movementRosie : MonoBehaviour
         
         if (Input.touchCount==2)
         {
-            pos=initial_pos;
+            dist=0;      
         }
         else if (Input.touchCount==1)
         {
             float spd=Time.deltaTime*moveSpd;
-
-            if (Vector2.Distance(new Vector2(pos.x,pos.z),new Vector2(targetPosition.x,targetPosition.z))<spd)
-            {
-                pos.x=targetPosition.x;
-                pos.z=targetPosition.z;
-            }
-            else
-            {
-                float ang=Mathf.Atan2(targetPosition.z-pos.z,targetPosition.x-pos.x);
-                pos.x+=spd*Mathf.Cos(ang);
-                pos.z+=spd*Mathf.Sin(ang);
-            }
+            dist+=spd;
         }
 
+        Vector3 path_pos=path.path.GetPointAtDistance(dist, EndOfPathInstruction.Stop);
+        path_pos=transform.parent.transform.InverseTransformPoint(path_pos);
+        pos.x=path_pos.x;
+        pos.z=path_pos.z;
+
         transform.localPosition=pos;
-        
     }
 }
